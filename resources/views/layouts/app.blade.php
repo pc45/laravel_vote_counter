d159ec9fa8221649b88e62371042ddd1eba43d97
Pete Croke <pete@petecroke.com>
2021-03-24 12:19:18 -0400

    lesson 4 updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,45 @@
 
         <main class="container mx-auto max-w-custom flex">
             <div class="w-70 mr-5">
-                add idea form
+                <div class="bg-white border-2 border-blue rounded-xl mt-16">
+                    <div class="text-center px-6 py-2 pt-6">
+                        <h3 class="font-semibold text-base">Add an Idea</h3>
+                        <p class="text-xs mt-4">Let us know what'd you would like and we'll take a look over!</p>
+                    </div>
+
+                    <form action="#" class="space-y-4 px-4 py-6" method="post">
+                        <div>
+                            <input type="text" class="text-sm border-none w-full bg-gray-100 rounded-xl placeholder:gray-900 px-4 py-2" placeholder="Your Idea" >
+                        </div>
+
+                        <div>
+                            <select name="category_add" id="category_add" class="bg-gray-100 text-sm border-none w-full rounded-xl px-4 py-2 border-none">
+                                <option value="Category One">Category One</option>
+                                <option value="Category Two">Category Two</option>
+                                <option value="Category Three">Category Three</option>
+                                <option value="Category Four">Category Four</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <textarea class="w-full bg-gray-100 rounded-xl placeholder:gray-900 text-sm px-4 py-2 border-none" placeholder="Describe your idea" name="idea" id="idea" cols="30" rows="4"></textarea>
+                        </div>
+
+                        <div class="flex items-center justify-between space-x-3">
+                            <button type="button" class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                                <svg class="text-gray-600 w-4 -rotate-45 transform" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                                </svg>
+                                <span class="ml-1">Attach</span>
+                            </button>
+
+                            <button type="submit" class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue font-semibold rounded-xl border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3 text-white">
+                                <span class="ml-1">Submit</span>
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
             </div>
             <div class="w-175">
                 <nav class="flex items-center justify-between text-xs">
