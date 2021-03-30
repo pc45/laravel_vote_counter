0b8e794930bf5ae99398c0ddf330f364ffeff79f
Pete Croke <pete@petecroke.com>
2021-03-30 17:04:38 -0400

    episode 11 - idea container link

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -29,7 +29,20 @@
 
    <div class="ideas-container space-y-6 my-6">
       @foreach ($ideas as $idea)
-        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+        <div
+            x-data
+            @click="
+                const clicked = $event.target
+
+                const target = clicked.tagName.toLowerCase()
+
+                const ignores = ['button','svg','path','a']
+
+                if (!ignores.includes(target)){
+                    clicked.closest('.idea-container').querySelector('.idea-link').click()
+                }
+            "
+            class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
          <div class="hidden md:block border-r border-gray-100 px-5 py-8">
             <div class="text-center">
                <div class="font-semibold text-2xl">
@@ -53,7 +66,7 @@
             </div>
             <div class="w-full mx-4 flex flex-col justify-between">
                <h4 class="text-xl font-semibold mt-2 md:mt-0">
-                   <a href="{{route('idea.show',$idea)}}" class="hover:underline">{{ $idea->title }}</a>
+                   <a href="{{route('idea.show',$idea)}}" class="idea-link hover:underline">{{ $idea->title }}</a>
                </h4>
                <div class="text-gray-600 mt-3 line-clamp-3">{{ $idea->description }}</div>
                 <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
