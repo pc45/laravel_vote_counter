c972c19e16d36db3dacd44c65c0c56d4ba933633
Pete Croke <pete@petecroke.com>
2021-04-14 11:29:41 -0400

    episode 19 idea box updates

--- resources/views/livewire/idea-index.blade.php
+++ resources/views/livewire/idea-index.blade.php
@@ -14,7 +14,7 @@
     class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
     <div class="hidden md:block border-r border-gray-100 px-5 py-8">
         <div class="text-center">
-            <div class="font-semibold text-2xl">
+            <div class="font-semibold text-2xl @if($hasVoted) text-blue @endif">
                 {{$votesCount}}
             </div>
             <div class="text-gray-500">
@@ -23,7 +23,11 @@
         </div>
 
         <div class="mt-8">
-            <button class="w-20 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in bg-gray-200 font-bold text-xxs uppercase rounded-xl px-4 py-3">Vote</button>
+            @if($hasVoted)
+                <button class="w-20 border text-white border-blue hover:border-gray-400 transition duration-150 ease-in bg-blue font-bold text-xxs uppercase rounded-xl px-4 py-3">Voted</button>
+            @else
+                <button class="w-20 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in bg-gray-200 font-bold text-xxs uppercase rounded-xl px-4 py-3">Vote</button>
+            @endif
         </div>
     </div>
 
@@ -68,11 +72,15 @@
 
                 <div class="flex items-center md:hidden mt-4 md:mt-0">
                     <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
-                        <div class="text-sm font-bold leading-none">{{$votesCount}}</div>
+                        <div class="text-sm font-bold leading-none @if($hasVoted) text-blue @endif">{{$votesCount}}</div>
                         <div class="text-xxs font-semibold leading-none text-gray-400 ">Votes</div>
                     </div>
 
-                    <button class="w-20 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in bg-gray-200 font-bold text-xxs uppercase rounded-xl px-4 py-3 -mx-5">Vote</button>
+                    @if($hasVoted)
+                        <button class="w-20 border border-blue text-white hover:border-gray-400 transition duration-150 ease-in bg-gray-200 font-bold text-xxs uppercase rounded-xl px-4 py-3 -mx-5">Voted</button>
+                    @else
+                        <button class="w-20 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in bg-gray-200 font-bold text-xxs uppercase rounded-xl px-4 py-3 -mx-5">Vote</button>
+                    @endif
                 </div>
             </div>
         </div>
