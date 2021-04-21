9ac4f874cbc536614e52ba27a509dfb9037b9879
Pete Croke <pete@petecroke.com>
2021-04-20 23:44:47 -0400

    make livewire component for filters

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -3,24 +3,33 @@
 namespace App\Http\Livewire;
 
 use App\Models\Idea;
+use App\Models\Status;
 use App\Models\Vote;
 use Livewire\Component;
 use Livewire\WithPagination;
 
-class IdeasIndex extends Component
-{
-    use WithPagination;
+class IdeasIndex extends Component {
+
+    // use WithPagination;
 
     public function render()
     {
-        return view('livewire.ideas-index', [
-            'ideas'=> Idea::with('user','category','status')
-                ->addSelect(['voted_by_user' => Vote::select('id')
-                    ->where('user_id',auth()->id())
-                    ->whereColumn('idea_id', 'ideas.id') ])
-                ->withCount('votes')
-                ->orderby('id','desc')
-                ->simplePaginate(Idea::PAGINATION_COUNT),
-        ]);
+        $statuses = Status::all()->pluck( 'id', 'name' );
+
+        return view( 'livewire.ideas-index', [
+            'ideas' => Idea::with( 'user', 'category', 'status' )
+                           ->when( request()->status && request()->status !== 'All', function ( $query ) use ( $statuses ) {
+                               return $query->where( 'status_id',
+                                   $statuses->get( request()->status ) );
+                           } )
+                           ->addSelect( [
+                               'voted_by_user' => Vote::select( 'id' )
+                                                      ->where( 'user_id', auth()->id() )
+                                                      ->whereColumn( 'idea_id', 'ideas.id' ),
+                           ] )
+                           ->withCount( 'votes' )
+                           ->orderBy( 'id', 'desc' )
+                           ->simplePaginate( Idea::PAGINATION_COUNT ),
+        ] );
     }
 }
